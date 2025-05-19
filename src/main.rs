76d81b69d412dd20800bba658aea9c07e416f9b2
Bruce Wayne <admin@bige0.com>
2025-05-19 10:26:59 +0800

    feat: initial

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use actix_web::http::StatusCode;
+use actix_web::HttpRequest;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use std::env;
+use std::net::{IpAddr, Ipv6Addr};
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[get("/")]
+async fn get_client_ip(req: HttpRequest) -> (String, StatusCode) {
+    let get_xff = |header_name: &str| -> Option<IpAddr> {
+        let ip = req.headers().get(header_name)?.to_str().ok()?;
+        let ip = ip.split(',').next()?;
+        let ip = ip.trim();
+
+        ip.parse::<IpAddr>().ok()
+    };
+
+    if let Some(ip) = get_xff("X-Forwarded-For") {
+        return (ip.to_string(), StatusCode::OK);
+    }
+
+    // unreachable!()
+    if let Some(addr) = req.peer_addr() {
+        return (
+            addr.ip().to_canonical().to_string(),
+            StatusCode::INTERNAL_SERVER_ERROR,
+        );
+    }
+
+    (String::default(), StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = env::var("RSIP_PORT_NUMBER").unwrap_or_default();
+    let port = port.parse::<u16>().unwrap_or(80);
+
+    HttpServer::new(|| App::new().service(health).service(get_client_ip))
+        .bind((Ipv6Addr::UNSPECIFIED, port))?
+        .run()
+        .await
+}
+
+#[cfg(test)]
+mod tests;
---
+++ src/tests.rs
@@ -0,0 +1,31 @@
+use actix_web::{http::StatusCode, test, App};
+
+use super::*;
+
+#[actix_web::test]
+async fn test_health() {
+    let app = test::init_service(App::new().service(health)).await;
+    let req = test::TestRequest::get().uri("/health").to_request();
+    let resp = test::call_service(&app, req).await;
+
+    assert_eq!(resp.status(), StatusCode::OK);
+    assert!(resp.headers().is_empty());
+
+    let body = test::read_body(resp).await;
+    assert!(body.is_empty());
+}
+
+#[actix_web::test]
+async fn test_get_client_ip() {
+    let app = test::init_service(App::new().service(get_client_ip)).await;
+    let req = test::TestRequest::default()
+        .insert_header(("X-Forwarded-For", "::2, 1.1.1.1"))
+        .to_request();
+    let resp = test::call_service(&app, req).await;
+
+    assert_eq!(resp.status(), StatusCode::OK);
+
+    let body = test::read_body(resp).await;
+    let body = std::str::from_utf8(&body).unwrap();
+    assert_eq!(body, "::2");
+}
